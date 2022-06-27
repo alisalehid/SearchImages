c69106f5f018de29308b8ede09669361cfc5ad1b
Ali Salehi <alisalehi.d@gmail.com>
2022-06-27 12:27:05 +0430

    "pagination"

--- data/src/main/java/com/payback/data/Constants.kt
+++ data/src/main/java/com/payback/data/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val BASE_URL = BuildConfig.BASE_URL
     const val DATABASE_NAME = "app_db"
     const val DEFAULT_WORD = "fruits"
-    const val PAGE_START = 1
     const val ALL_PAGE = 25
     val KEY = Pair("key", BuildConfig.API_KEY)
     val IMAGE_TYPE = Pair("image_type", "photo")
